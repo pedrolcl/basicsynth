9812fdda47c57ad3f514c7bb8ec2e7f2e4a3f0cd
pheugo <>
2009-08-20 07:31:21 +0000

    final adjustments

--- Src/Examples/Example9a/main.cpp
+++ Src/Examples/Example9a/main.cpp
@@ -1,12 +1,16 @@
 /////////////////////////////////////////////////////////////////////
 // BasicSynth Example09a (Chapter 16)
 //
-// MIDI file loading and playback
+// MIDI file playback
 //
-// use: Example09 [-vn] soundfont.sf2 infile.mid outfile.wav
+// This program loads a GM soundbank (SF2 or DLS) and a MIDI file (.mid)
+// and produces a wave file.
+//
+// use: Example09 [-vn] [-s] soundfont.sf2 infile.mid outfile.wav
 // -v = master volume level
+// -s = silent output
 //
-// Copyright 2008, Daniel R. Mitchell
+// Copyright 2009, Daniel R. Mitchell
 /////////////////////////////////////////////////////////////////////
 #include <stdlib.h>
 #include <stdio.h>
@@ -23,103 +27,21 @@
 
 void useage()
 {
-	fprintf(stderr, "use: example9a [-vn] soundfount midifile wavfile\n");
+	fprintf(stderr, "use: example9a [-vn] [-s] soundbank midifile wavefile\n");
+	fprintf(stderr, "    -vn = master volume level, default 1.0\n");
+	fprintf(stderr, "    -s  = don't print info or time\n");
 	exit(1);
 }
 
+int verbose = 1;
 time_t clkTime = 0;
 long genTime = 0;
 void GenCallback(bsInt32 count, Opaque arg)
 {
-	fprintf(stderr, "Time %02d:%02d\r", (genTime / 60), (genTime %60));
+	if (verbose)
+		fprintf(stderr, "Time %02d:%02d\r", (genTime / 60), (genTime %60));
 	genTime++;
 }
-
-// Specialized instrument manager that bypasses the mixer.
-// We can do this because the volume levels are controlled
-// by the MIDI CC# 7 on a per-channel basis. The mixer inputs
-// are therefore, redunant.
-class GMInstrManager : public InstrManager
-{
-protected:
-	AmpValue outLft;
-	AmpValue outRgt;
-	AmpValue masterVol;
-//	GMManager *gm;
-
-public:
-	GMInstrManager()
-	{
-		masterVol = 1.0;
-//		gm = new GMManager;
-	}
-
-	~GMInstrManager()
-	{
-//		delete gm;
-	}
-
-	void SetVolume(AmpValue v)
-	{
-		masterVol = v;
-	}
-
-/*	virtual Instrument *Allocate(bsInt16 inum)
-	{
-		return GMManager::InstrFactory(this, gm);
-	}
-
-	virtual Instrument *Allocate(InstrConfig *in)
-	{
-		return GMManager::InstrFactory(this, gm);
-	}
-
-	virtual void Deallocate(Instrument *ip)
-	{
-		ip->Destroy();
-	}
-
-	virtual SeqEvent *ManufEvent(bsInt16 inum)
-	{
-		return GMManager::EventFactory(gm);
-	}
-
-	virtual SeqEvent *ManufEvent(InstrConfig *in)
-	{
-		return GMManager::EventFactory(gm);
-	}
-*/
-	/// Start is called by the sequencer when the sequence starts.
-	virtual void Start()
-	{
-		outLft = 0;
-		outRgt = 0;
-	}
-	
-	virtual void Tick()
-	{
-		wvf->Output2(masterVol * outLft, masterVol * outRgt);
-		outLft = 0;
-		outRgt = 0;
-	}
-
-	virtual void FxSend(int unit, AmpValue val)
-	{
-		// TODO: send to reverb/chorus
-	}
-
-	virtual void Output(int ch, AmpValue val)
-	{
-		outLft += val;
-		outRgt += val;
-	}
-
-	virtual void Output2(int ch, AmpValue lft, AmpValue rgt)
-	{
-		outLft += lft;
-		outRgt += rgt;
-	}
-};
 
 int main(int argc, char *argv[])
 {
@@ -134,8 +56,7 @@ int main(int argc, char *argv[])
 	WaveFile wvf;
 	Sequencer seq;
 	Mixer mix;
-	//InstrManager inmgr;
-	GMInstrManager inmgr;
+	InstrManager inmgr;
 	SFFile sf;
 	DLSFile dls;
 	SMFFile smf;
@@ -146,10 +67,16 @@ int main(int argc, char *argv[])
 
 	int argn = 1;
 	char *ap = argv[argn];
-	if (argc > 1 && *ap == '-' && ap[1] == 'v')
+	while (argn < argc && *ap == '-')
 	{
-		vol = atof(&ap[2]);
+		if (ap[1] == 'v')
+			vol = atof(&ap[2]);
+		else if (ap[1] == 's')
+			verbose = 0;
+		else
+			useage();
 		argn++;
+		ap = argv[argn];
 	}
 
 	if (argn < argc)
@@ -161,7 +88,6 @@ int main(int argc, char *argv[])
 	if (sbFile.Length() == 0 || midFile.Length() == 0 || wavFile.Length() == 0)
 		useage();
 
-	
 	SoundBank *sb = 0;
 	if (SFFile::IsSF2File(sbFile))
 		sb = sf.LoadSoundBank(sbFile, 0, 0.375);
@@ -173,21 +99,27 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
+	if (verbose)
+	{
+		printf("Soundbank:\n%s\nVersion %d.%d.%d.%d\n%s\n\n", (const char*)sb->info.szName, 
+			sb->info.wMajorFile, sb->info.wMinorFile, 
+			sb->info.wMajorVer, sb->info.wMinorVer, 
+			(const char*)sb->info.szCopyright);
+	}
+
 	if (smf.LoadFile(midFile))
 	{
 		fprintf(stderr, "Errror loading .mid file %s\n", (const char *)midFile);
 		exit(1);
 	}
 
+	if (verbose)
+		printf("Song:\n%s%s%s\n%s\n", smf.SeqName(), smf.Copyright(), smf.TimeSignature(), smf.KeySignature());
+
 	// This shows using one player config for all channels.
 	// When	using the GMManager, instruments get the patch
 	// directly from the MIDIChannelControl object. Envelope
-	// is set from the SF2 file.
-	// Alternatively, create a separate instrument for
-	// each channel, and set the bank, patch and envelope
-	// from the SeqEvent object passed in on start, or
-	// ignore the program change and go with a fixed
-	// synthesis sound on each channel.
+	// is set from the SF2/DLS file.
 
 	// Add GMManager type to instrument manager
 	InstrMapEntry *ime = inmgr.AddType("GMPlayer",
@@ -224,10 +156,6 @@ int main(int argc, char *argv[])
 		exit(0);
 	}
 
-	fprintf(stderr, "%s%s%s%s\n", smf.SeqName(), smf.Copyright(), smf.TimeSignature(), smf.KeySignature());
-
-	inmgr.SetVolume(vol);
-
 	// Initialize the mixer
 	mix.MasterVolume(vol, vol);
 	mix.SetChannels(nchnls);
@@ -235,9 +163,9 @@ int main(int argc, char *argv[])
 	{
 		mix.ChannelVolume(i, 1.0);
 		mix.ChannelOn(i, chnls[i] > 0);
-		//printf("%d events on channel %d\n", chnls[i], i);
 	}
 
+	// run the sequencer
 	wvf.OpenWaveFile(wavFile, 2);
 	inmgr.Init(&mix, &wvf);
 	seq.SetController(&mc.seqControl);
@@ -247,10 +175,13 @@ int main(int argc, char *argv[])
 	seq.Sequence(inmgr, 0, 0);
 	wvf.CloseWaveFile();
 
-	long clkTimeDiff = (long) (time(0) - clkTime);
-	printf("\n%02d:%02d in %02ld:%02ld %d%%\n", 
-		genTime / 60, genTime % 60, clkTimeDiff / 60, clkTimeDiff % 60,
-		(clkTimeDiff * 100) / genTime);
+	if (verbose)
+	{
+		long clkTimeDiff = (long) (time(0) - clkTime);
+		printf("\n%02d:%02d in %02ld:%02ld %d%%\n", 
+			genTime / 60, genTime % 60, clkTimeDiff / 60, clkTimeDiff % 60,
+			(clkTimeDiff * 100) / genTime);
+	}
 
 	return 0;
 }
